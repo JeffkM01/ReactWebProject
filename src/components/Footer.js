91e22b92d617941f5120dd436d689d91c5b82330
Jeff M <jotomwangi380@gmail.com>
2025-08-31 11:04:32 -0500

    Commiting deployment changes2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ const Footer = () => {
         <div class="row">
             <footer id = "footerLinks">
                 <div class="col-xs-12 col-sm-4" style = {{paddingBottom: "5px"}}>
-                    <a class="foot1" href="https://umsystem.joinhandshake.com/explore" target="_blank">Handshake</a>
+                    <a class="foot1" href="https://umsystem.joinhandshake.com/explore" target="_blank" rel="noreferrer">Handshake</a>
                 </div>
                 <div class="col-xs-12 col-sm-4" style = {{paddingBottom: "5px"}}>
-                    <a class="foot1" href="https://www.w3schools.com/" target="_blank">w3schools</a>
+                    <a class="foot1" href="https://www.w3schools.com/" target="_blank" rel="noreferrer">w3schools</a>
                 </div>
                 <div class="col-xs-12 col-sm-4" style = {{paddingBottom: "5px"}}>
-                    <a class="foot1" href="https://umsystem.instructure.com" target="_blank">Canvas</a>
+                    <a class="foot1" href="https://umsystem.instructure.com" target="_blank" rel="noreferrer">Canvas</a>
                 </div>
             </footer>
          </div>       
